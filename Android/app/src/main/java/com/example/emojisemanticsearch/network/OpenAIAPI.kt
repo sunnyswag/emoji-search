1611858b9a4e8ad5733c4a8b3613b918792202e4
sunnyswag <hhq126152@gmail.com>
2023-06-17 20:30:27 +0800

    add embedding.json to Android project

--- Android/app/src/main/java/com/example/emojisemanticsearch/network/OpenAIAPI.kt
+++ Android/app/src/main/java/com/example/emojisemanticsearch/network/OpenAIAPI.kt
@@ -1,9 +1,17 @@
 package com.example.emojisemanticsearch.network
 
+import com.example.emojisemanticsearch.entity.EmbeddingRequest
+import com.google.gson.JsonObject
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface OpenAIAPI {
-    @POST("engines/davinci/completions")
-    suspend fun getEmbedding(@Body text: String): String
+
+    @Headers(
+        "Content-Type:application/json",
+        "Authorization:Bearer API_KEY"
+    )
+    @POST("v1/embeddings")
+    suspend fun getEmbedding(@Body request: EmbeddingRequest): JsonObject
 }
